56e5c6b012d905a417d2a12770eb11108f5ec975
Aminjoni Abdullozoda <30694948+AminPlusPlus@users.noreply.github.com>
2018-08-12 23:33:08 +0200

    new modile
    d

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 const Card = props => {
   return (
     <div style={{ margin: "1em" }}>
-      <img src="http://placehold.it/75" />
+      <img width="75" src={props.avatarUrl} />
       <div
         style={{
           display: "inline-block",
@@ -14,20 +14,72 @@ const Card = props => {
           verticalAlign: "top"
         }}
       >
-        <div style={{ fontSize: "1.25em", fontWeight: "bold" }}>Name Here</div>
-        <div> Company Name here ... </div>
+        <div style={{ fontSize: "1.25em", fontWeight: "bold" }}>
+          {props.name}
+        </div>
+        <div> {props.company}</div>
       </div>
     </div>
   );
 };
 
 const CardList = props => {
-  return (
-    <div>
-      <Card />
-    </div>
-  );
+  return <div>{props.list.map(card => <Card {...card} />)}</div>;
 };
 
+class Form extends React.Component {
+  state = { userInput: "" };
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    console.log("sumpited : ", this.state.userInput);
+    //AJAX  fetch ..
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            value={this.state.userInput}
+            onChange={event => this.setState({ userInput: event.target.value })}
+            type="text"
+            placeholder="Search Github User"
+            required
+          />
+          <button type="submit"> Add User </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    data: [
+      {
+        name: "Aminjoni Abdullozoda",
+        avatarUrl:
+          "https://avatars3.githubusercontent.com/u/30694948?s=460&v=4",
+        company: "Google Inc"
+      },
+      {
+        name: "Milod Orif",
+        avatarUrl:
+          "https://avatars1.githubusercontent.com/u/25881325?s=460&v=4",
+        company: "Pornhub.com"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        <Form />
+        <CardList list={this.state.data} />
+      </div>
+    );
+  }
+}
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<CardList />, rootElement);
+ReactDOM.render(<App />, rootElement);
